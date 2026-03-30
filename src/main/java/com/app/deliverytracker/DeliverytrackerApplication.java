c263c36b3a21430ed343b152042c8717a89eabad
zainabalzaimoor <alzaimoorzainab@gmail.com>
2026-03-30 19:36:06 +0300

    Initial Spring Boot setup

---
+++ src/main/java/com/app/deliverytracker/DeliverytrackerApplication.java
@@ -0,0 +1,13 @@
+package com.app.deliverytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverytrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverytrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/deliverytracker/DeliverytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.deliverytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverytrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
